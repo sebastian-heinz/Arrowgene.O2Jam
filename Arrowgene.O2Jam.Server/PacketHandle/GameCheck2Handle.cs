136242b22d84e07baa3e61891804f7dd07ce3ced
sebastian-heinz <sebastian.heinz.gt@googlemail.com>
2021-10-06 19:45:25 +0800

    add vscode

--- Arrowgene.O2Jam.Server/PacketHandle/GameCheck2Handle.cs
+++ Arrowgene.O2Jam.Server/PacketHandle/GameCheck2Handle.cs
@@ -15,11 +15,36 @@ namespace Arrowgene.O2Jam.Server.PacketHandle
 
         public override void Handle(Client client, NetPacket packet)
         {
+            IBuffer req = packet.CreateReadBuffer();
+            uint un0 = req.ReadUInt32();
+            uint un1 = req.ReadUInt32();
+            uint time = req.ReadUInt32();
+            uint count = req.ReadUInt32();
+            uint un2 = req.ReadUInt32();
+
+            Logger.Info($"un0:{un0} un1:{un1} time:{time} count:{count} un2:{un2}");
+            
             IBuffer res = new StreamBuffer();
-            // client read 14bytes
-            byte[] data = new byte[14];
-            res.WriteBytes(data);
+            res.WriteUInt32(un0);
+            res.WriteUInt32(un1);
+            res.WriteUInt32(time);
+            res.WriteUInt32(count);
+            res.WriteUInt32(un2);
+
+
             client.Send(res.GetAllBytes(), PacketId.GameCheck2Res);
+
+
+          // if (un2 == 1)
+          // {
+          //     IBuffer res1 = new StreamBuffer();
+          //     res1.WriteBytes(new byte[20]);
+          //     for (ushort i = 4000; i < 4200; i++)
+          //     {
+          //         NetPacket p = new NetPacket(i, res1.GetAllBytes(), PacketSource.Server);
+          //         client.Send(p);
+          //     }
+          // }
         }
     }
 }
