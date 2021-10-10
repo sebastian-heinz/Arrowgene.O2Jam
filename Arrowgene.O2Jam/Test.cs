bda6d6538a714a5b09bc8023c64f359c3950c5ac
sebastian-heinz <sebastian.heinz.gt@googlemail.com>
2021-10-10 17:50:55 +0800

    start decrypt args

--- Arrowgene.O2Jam/Test.cs
+++ Arrowgene.O2Jam/Test.cs
@@ -28,8 +28,9 @@ namespace Arrowgene.O2Jam
                 String strVal = test.Substring(i, i + 6);
                 int intVal = Int32.Parse(strVal, NumberStyles.AllowHexSpecifier);
                 double p1 = (double) intVal;
-
                 float res = fun_585a80(p1, p2, p3);
+                
+                
 
                 int lol = 1;
             }
@@ -44,31 +45,64 @@ namespace Arrowgene.O2Jam
             uint local_30 = (uint) (long) Math.Round(p1);
             uint local_20 = local_30;
             int local_14 = 1;
-            uint lcoal_40 = (uint) (long) Math.Round(p2);
-            uint lcoal_10 = lcoal_40;
+            uint local_40 = (uint) (long) Math.Round(p2);
+            uint local_10 = local_40;
             float fVar3;
 
             while (true)
             {
                 if ((bVar2) || (local_20 == 1))
                 {
-                    break;
+                    // LABEl
+                    fVar3 = (float) fun_5fa5c0((double) (ulong) (local_14 * local_20), p3);
+                    return (float) ((long) Math.Round(fVar3) & 0xFFFFFFFF);
                 }
 
-                fVar3 = (float) fun_58dc0((double) (ulong) local_20, p3);
+                fVar3 = (float) fun_585dc0((double) (ulong) local_20, p3);
                 ulong uVar1 = (ulong) Math.Round(fVar3);
                 uint local_58 = (uint) uVar1;
                 if (local_58 == 0)
                 {
                     return (float) -1.0;
                 }
+
+                fVar3 = (float) fun_5fa5c0((double) (ulong) local_10, (double) (uVar1 & 0xFFFFFFFF));
+                int local_78 = (int) (long) Math.Round(fVar3);
+                local_10 = (uint) ((local_10 - local_78) / local_58);
+                double dVar4 = Math.Pow((double) (ulong) local_20, (double) ((long) Math.Round(fVar3) & 0xFFFFFFFF));
+                int local_98 = (int) (long) Math.Round(dVar4);
+                fVar3 = (float) fun_5fa5c0((double) (ulong) (uint) (local_98 * local_14), p3);
+                int local_b0 = (int) (long) Math.Round(fVar3);
+                local_14 = local_b0;
+                if (local_10 == 0)
+                {
+                    break;
+                }
+
+                if (local_10 == 1)
+                {
+                    bVar2 = true;
+                }
+
+                double uVar5 = p3;
+                dVar4 = Math.Pow((double) (long) local_20, (double) (uVar1 & 0xFFFFFFFF));
+                fVar3 = (float) fun_5fa5c0(dVar4, uVar5);
+                uint local_d0 = (uint) (long) Math.Round(fVar3);
+                local_20 = local_d0;
             }
 
+            local_20 = 1;
+            // LABEl
+            fVar3 = (float) fun_5fa5c0((double) (ulong) (local_14 * local_20), p3);
             return (float) ((long) Math.Round(fVar3) & 0xFFFFFFFF);
         }
 
+        private float fun_5fa5c0(double p1, double p2)
+        {
+            return (float)(p1 % p2);
+        }
 
-        private float fun_58dc0(double p1, double p2)
+        private float fun_585dc0(double p1, double p2)
         {
             float fVar1;
             if (p1 == 1.0)
@@ -105,11 +139,6 @@ namespace Arrowgene.O2Jam
             }
 
             return fVar1;
-        }
-
-
-        public void mod(byte[] ar)
-        {
         }
     }
 }
